ab6d7b3afdb862e1c621d1e6386c42d5493dca93
Amlan Sarmah <amlansarmah26@gmail.com>
2024-04-12 18:47:21 +0530

    Uploaded all files

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://axonstudio.tech',
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
+});
