1ad14192f4c43a67da38375b508cebe139bfdbfe
Amlan Sarmah <amlansarmah26@gmail.com>
2024-06-28 10:29:35 +0530

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://axonstudio.tech',
-  experimental: {
-    viewTransitions: true,
-  },
+  adapter: vercel(),
+  site: 'https://axonstudio.tech',
+  output: 'server', // Use 'server' to enable server-side rendering
+  // experimental: {
+  //   viewTransitions: true,
+  // },
   integrations: [tailwind(), compress()],
 });
