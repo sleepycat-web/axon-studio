034177863d46cd05e2515df62bcb569e61a3ec80
Amlan Sarmah <amlansarmah26@gmail.com>
2024-09-26 14:27:27 +0530

    add wad idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/serverless';
-
+ 
 // https://astro.build/config
 export default defineConfig({
    site: 'https://sleepycat-web.github.io',
